2abd584fc26c19abcc86b2a5158ba64042dc854d
mtaher3 <mtaher3@gatech.edu>
2024-12-11 10:03:17 -0800

    Coming Soon page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">Hello, Worlds!</h1>
-      <p className="text-lg text-white mt-4">Welcome to my portfolio page.</p>
+      <h1 className="text-4xl font-bold text-white">Coming soon!</h1>
+      {/* <p className="text-lg text-white mt-4">Welcome to my portfolio page.</p>
       <a
         href="#"
         className="mt-6 px-4 py-2 bg-white text-blue-600 rounded-lg shadow-lg hover:bg-gray-100"
       >
         Get Started
-      </a>
+      </a> */}
     </div>
   );
 }
